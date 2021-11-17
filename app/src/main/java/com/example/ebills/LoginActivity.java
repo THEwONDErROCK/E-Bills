31a8e2328049c0f5a73d1070fe98316c3fd10356
harsh kumar singh <thakurhksingh310@gmail.com>
2021-11-17 17:55:30 +0530

    sign-up and registeration page

--- app/src/main/java/com/example/ebills/LoginActivity.java
+++ app/src/main/java/com/example/ebills/LoginActivity.java
@@ -5,15 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.view.View;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private String s="Shbh";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         TextView btn = findViewById(R.id.signuplogin);
-        btn.setOnClickListener(v -> startActivity(new Intent( LoginActivity.this,RegisterActivity.class)));
+        TextView bt=findViewById(R.id.blogin);
+
+
+
+    }
+    public void sig(View v)
+    {
+        Intent in=new Intent(LoginActivity.this,RegisterActivity.class);
+        startActivity(in);
+
+    }
+    public void log(View v)
+    {
+        Intent in=new Intent(LoginActivity.this,MainActivity.class);
+        in.putExtra("name",s);
+
+        startActivity(in);
+
     }
 }
